b7be5d57eefa3a65f81285daa60e05c23f2c75d0
Coookie <timohuhnholz@gmail.com>
2022-10-29 11:13:25 +0200

    Belongs to the earlier Loco Simulation commit, yea

---
+++ DVMultiplayerContinued/Patches/Train/LocoSimulationPatch.cs
@@ -0,0 +1,61 @@
+﻿using DVMultiplayer.Networking;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVMultiplayer.Patches.Train
+{
+    [HarmonyPatch(typeof(ShunterLocoSimulation), "SimulateTick")]
+    class ShunterSimulationTickPatch
+    {
+        static bool Prefix(LocoSimulation __instance)
+        {
+            if (NetworkManager.IsClient())
+            {
+                NetworkTrainPosSync networking = __instance.GetComponent<NetworkTrainPosSync>();
+                if (networking)
+                {
+                    return networking.hasLocalPlayerAuthority;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(DieselLocoSimulation), "SimulateTick")]
+    class DieselSimulationTickPatch
+    {
+        static bool Prefix(LocoSimulation __instance)
+        {
+            if (NetworkManager.IsClient())
+            {
+                NetworkTrainPosSync networking = __instance.GetComponent<NetworkTrainPosSync>();
+                if (networking)
+                {
+                    return networking.hasLocalPlayerAuthority;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SteamLocoSimulation), "SimulateTick")]
+    class SteamSimulationTickPatch
+    {
+        static bool Prefix(LocoSimulation __instance)
+        {
+            if (NetworkManager.IsClient())
+            {
+                NetworkTrainPosSync networking = __instance.GetComponent<NetworkTrainPosSync>();
+                if (networking)
+                {
+                    return networking.hasLocalPlayerAuthority;
+                }
+            }
+            return true;
+        }
+    }
+}
